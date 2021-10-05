0f04b1a7a4cbad76af3d420d4c0d21789ea6c140
Thoriquee <thoriqabidin.2019@student.uny.ac.id>
2021-10-05 20:41:56 +0700

    Tambahan

--- lib/main.dart
+++ lib/main.dart
@@ -9,21 +9,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo 2',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: "Coba",
+        home: Scaffold(
+            appBar: AppBar(title: Text("Coba")),
+            body: Row(
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    color: Colors.yellow,
+                    child: Text("Ini nanti diisi gambar"),
+                  ),
+                ),
+                Expanded(
+                    flex: 6,
+                    child: Column(
+                      children: [
+                        Expanded(
+                            child: Row(children: [
+                          Expanded(
+                              flex: 5, child: Container(color: Colors.green)),
+                          Expanded(flex: 5, child: Container(color: Colors.red))
+                        ])),
+                        Expanded(
+                            child: Container(
+                          color: Colors.blue,
+                        )),
+                      ],
+                    ))
+              ],
+            )));
   }
 }
 
