e5d551cec32e5f0ecbf0d7f2bc7797a8990cef1b
Thoriquee <thoriqabidin.2019@student.uny.ac.id>
2021-11-02 11:16:24 +0700

    Login Page

---
+++ lib/loginScreen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                      Color(0xff5ac18e),
+                      Color(0xff5ac18e),
+                      Color(0xff5ac18e),
+                      Color(0xff5ac18e),
+                    ])),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
